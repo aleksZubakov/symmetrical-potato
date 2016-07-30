116e946b91b8d5a90da204cc7c812e9e975719c6
Mikhail Logachev <miha.loga@gmail.com>
2016-07-30 20:23:12 +0300

    Start Handler

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from  __future__ import print_function
+import logging
+import credentials
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 
-from telegram.ext import Updater, MessageHandler, Filters
+# setup updater, dispatcher, and logging
+updater = Updater( token=credentials.token )
+dispatcher = updater.dispatcher
+logging.basicConfig( format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.WARNING )
+
+# wrappers
+def addHandlers( handlers ):
+    for handler in handlers:
+        dispatcher.add_handler( handler )
+
+
+# def produceHandlers(  )
+
 
 
 def start(bot, update):
@@ -27,5 +43,15 @@ def grandfather_freid(bot, update):
     chat_id = update.messsage.chat_id
     bot.sendMessage(chat_id=chat_id, text=message_text)
 
-
+# handlers
+onStartCommand = CommandHandler( 'start', start )
 freid_handler = MessageHandler([Filters.text], grandfather_freid)
+
+# assign handlers
+addHandlers( [ freid_handler, onStartCommand ] )
+
+
+# start your engines
+updater.start_polling()
+
+
